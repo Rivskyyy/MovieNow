ffbd956c6995386f01fff8cf582c5474ab27c234
Taras Bandrivskyi <taras.bandrivskyi@gmail.com>
2022-10-11 04:11:33 +0300

    added png for FavorButton, Result = null, activity_movie_detail.xml added

--- app/src/main/java/com/example/movienow/API_SERVICE/Result.kt
+++ app/src/main/java/com/example/movienow/API_SERVICE/Result.kt
@@ -5,35 +5,37 @@ import androidx.room.PrimaryKey
 import com.google.gson.annotations.SerializedName
 
 @Entity(tableName = "favourite_movies")
-data class Result(
-    @SerializedName("adult")
-    val adult: Boolean,
-    @SerializedName("backdrop_path")
-    val backdrop_path: String,
-    @SerializedName("genre_ids")
-    val genre_ids: List<Int>,
+ data class Result(
+//    @SerializedName("adult")
+//    val adult: Boolean,
+//    @SerializedName("backdrop_path")
+//    val backdrop_path: String,
+//    @SerializedName("genre_ids")
+//    val genre_ids: List<Int>,
     @PrimaryKey
     @SerializedName("id")
-    val id: Int,
-    @SerializedName("original_language")
-    val original_language: String,
-    @SerializedName("original_title")
-    val original_title: String,
+    var id: Int?,
+//    @SerializedName("original_language")
+//    val original_language: String,
+//    @SerializedName("original_title")
+//    val original_title: String,
     @SerializedName("overview")
-    val overview: String,
-    @SerializedName("popularity")
-    val popularity: Double,
+    var overview: String?,
+//    @SerializedName("popularity")
+//    val popularity: Double,
     @SerializedName("poster_path")
-    val poster_path: String,
+    var poster_path: String?,
     @SerializedName("release_date")
-    val release_date: String,
+    var release_date: String?,
     @SerializedName("title")
-    val title: String,
-    @SerializedName("video")
-    val video: Boolean,
+    var title: String?,
+//    @SerializedName("video")
+//    val video: Boolean,
     @SerializedName("vote_average")
-    val vote_average: Double,
-    @SerializedName("vote_count")
-    val vote_count: Int
+    var vote_average: Double?,
+//    @SerializedName("vote_count")
+//    val vote_count: Int?
+
 
 )
+
