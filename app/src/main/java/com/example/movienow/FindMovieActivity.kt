674ddda8a1d93481b7efadccb7f0d019b71e5752
Taras Bandrivskyi <taras.bandrivskyi@gmail.com>
2022-10-11 04:07:42 +0300

    fixed adapter, added intent to FindMovieActivity

--- app/src/main/java/com/example/movienow/FindMovieActivity.kt
+++ app/src/main/java/com/example/movienow/FindMovieActivity.kt
@@ -1,44 +1,35 @@
 package com.example.movienow
 
-import android.content.Context
+
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.widget.Button
-import android.widget.HorizontalScrollView
-import android.widget.Toast
-import androidx.compose.ui.Modifier.Companion.any
-import androidx.core.view.isEmpty
-import androidx.core.view.size
-
-
+import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.Observer
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
-import com.example.movienow.API_SERVICE.API
-import com.example.movienow.API_SERVICE.API_Data
-
 import com.example.movienow.API_SERVICE.ApiFactory
 import com.example.movienow.API_SERVICE.Result
 import com.example.movienow.adapter.MoviesAdapter
-
+import com.example.movienow.database.MovieDao
+import com.example.movienow.database.MovieDataBase
 import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers
 import io.reactivex.rxjava3.functions.Consumer
 import io.reactivex.rxjava3.schedulers.Schedulers
-import java.net.URL
-
 import java.util.*
 import kotlin.random.Random
-import kotlin.random.nextInt
 
 
 class FindMovieActivity : AppCompatActivity() {
 
     private lateinit var buttonNextMovie: Button
-
     private lateinit var recyclerView: RecyclerView
     private lateinit var adapter: MoviesAdapter
-
+    private lateinit var viewModel: MainViewModel
+    private lateinit var dataBase: MovieDataBase
+    //private lateinit var imageFavorite: ImageButton
+    private lateinit var movieDao: MovieDao
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -48,34 +39,68 @@ class FindMovieActivity : AppCompatActivity() {
 
         loadMovies()
 
-        recyclerView.layoutManager = LinearLayoutManager(
+        recyclerView.layoutManager = LinearLayoutManager(           // -> for future catalog
+
             this,
             LinearLayoutManager.HORIZONTAL, false
         )
 
         recyclerView.adapter = adapter
 
+        adapter.onMovieClickListener_object = object : MoviesAdapter.onMovieClickListener {
+            override fun onMovieClick(movie: Result) {
+                intent = MovieDetailActivity.NewIntent(this@FindMovieActivity, movie).also {
+                    intent.putExtra("movie.title", movie.title)
+                }
+//                    intent.putExtra(EXTRA_MOVIE,movie.release_date)
+//                    intent.putExtra(EXTRA_MOVIE, movie.id)
+//                    intent.putExtra(EXTRA_MOVIE, movie.overview)
+//                    intent.putExtra(EXTRA_MOVIE, movie.title)
+//                    intent.putExtra(EXTRA_MOVIE, movie.poster_path)
+//                    intent.putExtra(EXTRA_MOVIE, movie.release_date)
+//                    intent.putExtra(EXTRA_MOVIE, movie.vote_average)
+                startActivity(intent)
+            }
+        }
         buttonNextMovie.setOnClickListener {
+
             loadMovies()
 
 
         }
+
     }
 
 
     private fun initViewFindMovie() {
         buttonNextMovie = findViewById(R.id.buttonNextMovie)
         recyclerView = findViewById(R.id.recyclerView)
+
+        //imageFavorite = findViewById(R.id.imageButtonFavor)
         adapter = MoviesAdapter()
+
     }
 
 
     fun loadMovies() {
+//        fun uploadOptimization(result: Result){
+//            if ( result.overview.length==null){
+//                loadMovies()
+//            } else {
+//                0
+//            }
+//        }
+//        var starOff: = (
+//            this,
+//            R.drawable.heart_icon_empty_resize)
+//
+//        var starOn: Drawable? = ContextCompat.getDrawable(
+//            this,
+//            R.drawable.heart_button_red)
 
+        val showResults = adapter.itemCount.toString()
 
-       val showResults =  adapter.itemCount.toString()
-
-        Log.d("TEST ITEMS" ,showResults.toString())
+        Log.d("TEST ITEMS", showResults.toString())
 
         val vote_min = 500
         val vote_max = 1000000
@@ -84,44 +109,43 @@ class FindMovieActivity : AppCompatActivity() {
         val setupAverageMin = intent.getStringExtra("averageMin")?.toInt()
         val setupAverageMax = intent.getStringExtra("averageMax")?.toInt()
         val setupPageForAllGenre = intent.getStringExtra("setupPageForAllGenre")?.toInt()
-       // val setupPageForAllGenres = intent.getStringExtra("genre")?.toInt() // use when genre is null
+        // val setupPageForAllGenres = intent.getStringExtra("genre")?.toInt() // use when genre is null
 
         val voteAverageMin = setupAverageMin
         val voteAverageMax = setupAverageMax
         val genre: Int? = setupGenre
 
-        var page  =
-        try {
+        var page =
+            try {
 
-            let {
-                if (genre!!.equals(27))  {
-                    Random.nextInt(1, 41)       // horror
-                }  else if   (genre.equals(28) ) {
-                    Random.nextInt(1,40)         // action
-                } else if (genre.equals(35)) {
-                    Random.nextInt(1,40)        //comedy
-                } else if (genre.equals(18)){
-                    Random.nextInt(1,40)        //drama
-                } else if (genre.equals(14)){
-                    Random.nextInt(1,37)        //fantasy
-                } else if (genre.equals(53)){
-                    Random.nextInt(1,40)        //thriller
-                } else if (genre.equals(16)){
-                    Random.nextInt(1,27)        //animation
-                  // all
-                } else{
-                      0
+                let {
+                    if (genre!!.equals(27)) {
+                        Random.nextInt(1, 41)       // horror
+                    } else if (genre.equals(28)) {
+                        Random.nextInt(1, 40)         // action
+                    } else if (genre.equals(35)) {
+                        Random.nextInt(1, 40)        //comedy
+                    } else if (genre.equals(18)) {
+                        Random.nextInt(1, 40)        //drama
+                    } else if (genre.equals(14)) {
+                        Random.nextInt(1, 37)        //fantasy
+                    } else if (genre.equals(53)) {
+                        Random.nextInt(1, 40)        //thriller
+                    } else if (genre.equals(16)) {
+                        Random.nextInt(1, 27)        //animation
+                        // all
+                    } else {
+                        0
+                    }
+
+                }
+            } catch (e: Exception) {
+                Log.d("EXCEPTION : ", e.message.toString())
             }
 
-            }
-        } catch (e:Exception){
-            Log.d("EXCEPTION : ", e.message.toString())
+        if (genre == null) {
+            page = Random.nextInt(1, 100)       //all
         }
-
-            if ( genre == null){
-                page = Random.nextInt(1, 100)       //all
-            }
-
 
 
         var language: String = Locale.getDefault().language.toString()
@@ -133,7 +157,7 @@ class FindMovieActivity : AppCompatActivity() {
         //val region: String = Locale.getDefault().country.toString()
 
         Log.d("Language: ", language.toString())
-       // Log.d("Region:  ,", region.toString())
+        // Log.d("Region:  ,", region.toString())
         // need more total results, something get wrong and results get less than must be
         // fixed, the region's fault
 
@@ -143,7 +167,7 @@ class FindMovieActivity : AppCompatActivity() {
         val apiFactory = ApiFactory.apiService.loadMovies(
             page,
             language,
-           // region,
+            // region,
             vote_min,
             vote_max,
             genre,
@@ -155,7 +179,9 @@ class FindMovieActivity : AppCompatActivity() {
             .observeOn(AndroidSchedulers.mainThread())
             .subscribe(Consumer {
                 Log.d("Test", it.toString())
-                adapter.setMovies(it.results)
+                val dataMovies = adapter.setMovies(it.results)
+
+
             }, {
 
                 Log.d("Test_Fail", it.message.toString())
--- app/src/main/java/com/example/movienow/adapter/MoviesAdapter.kt
+++ app/src/main/java/com/example/movienow/adapter/MoviesAdapter.kt
@@ -1,61 +1,106 @@
 package com.example.movienow.adapter
 
+import android.app.Application
+import android.content.Context
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
+import android.widget.Toast
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.LifecycleOwner
+import androidx.lifecycle.LiveData
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.example.movienow.API_SERVICE.BASE_IMAGE_POSTER
 import com.example.movienow.API_SERVICE.Result
+import com.example.movienow.FindMovieActivity
+import com.example.movienow.MainViewModel
 import com.example.movienow.R
+import com.example.movienow.database.MovieDao
+import com.example.movienow.database.MovieDao_Impl
+import com.example.movienow.database.MovieDataBase
+import com.example.movienow.database.MovieDataBase_Impl
+import io.reactivex.rxjava3.schedulers.Schedulers
 import java.util.*
-import kotlin.collections.ArrayList
-import kotlin.random.Random
-import kotlin.random.nextInt
 
+public class MoviesAdapter() : RecyclerView.Adapter<MoviesAdapter.MyViewHolder>() {
 
-class MoviesAdapter() : RecyclerView.Adapter<MoviesAdapter.MyViewHolder>() {
 
     private var movies: List<Result> = ArrayList<Result>()
+
+
+    var onMovieClickListener_object: onMovieClickListener? = null
+
+
+    private lateinit var imageView: ImageView
+
+
     fun setMovies(movies: List<Result>) {
         this.movies = movies
         notifyDataSetChanged()
         Collections.shuffle(movies)
-
     }
 
 
-    class MyViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+    class MyViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView), LifecycleOwner {
+
         private lateinit var posterImageView: ImageView
         private lateinit var nameOfMovie: TextView
-        private lateinit var yearOfMovie : TextView
-        private lateinit var descriptionOfMovie : TextView
-        private lateinit var rating : TextView
-
+        //        private lateinit var yearOfMovie: TextView
+//        private lateinit var descriptionOfMovie: TextView
+        private lateinit var rating: TextView
+        private lateinit var viewModel: MainViewModel
 
         fun bindMovie(movie: Result) {
 
             posterImageView = itemView.findViewById(R.id.posterImage)
             nameOfMovie = itemView.findViewById(R.id.nameOfMovie)
-            yearOfMovie = itemView.findViewById(R.id.yearOfMovie)
-            descriptionOfMovie = itemView.findViewById(R.id.descriptionOfMovie)
+//            yearOfMovie = itemView.findViewById(R.id.yearOfMovie)
+//            descriptionOfMovie = itemView.findViewById(R.id.descriptionOfMovie)
             rating = itemView.findViewById(R.id.rating)
+//            imageFavorite = itemView.findViewById(R.id.imageButtonFavor)
 
+            val getId = movie.id
 
 
             rating.text = movie.vote_average.toString()
-            descriptionOfMovie.text = movie.overview
-            yearOfMovie.text = movie.release_date
+//            descriptionOfMovie.text = movie.overview
+//            yearOfMovie.text = movie.release_date
             nameOfMovie.text = movie.title
+            var id = movie.id
             Glide.with(itemView).load(BASE_IMAGE_POSTER + movie.poster_path).into(posterImageView)
 
+            fun getDataBaseMovie() {
+                val itemMovieData = com.example.movienow.API_SERVICE.Result(
+                    movie.id, movie.overview,
+                    movie.poster_path, movie.release_date,
+                    movie.title, movie.vote_average
+                )
+                Log.d("DATA", getDataBaseMovie().toString())
+
+                fun getContext(context: Context) {
+                    var movieDao: MovieDao
+                    movieDao = MovieDataBase.getInstance(context).movieDao()
+                    movieDao.getAllFavouriteMovie().observe(this, androidx.lifecycle.Observer {
+                        movieDao.insertMovie(itemMovieData).subscribeOn(Schedulers.io()).subscribe()
+
+                    })
+                }
+            }
+
+        }
+
+        override fun getLifecycle(): Lifecycle {
+
+            return lifecycle
         }
 
 
     }
+
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
         val view = LayoutInflater.from(parent.context).inflate(R.layout.movie_items, parent, false)
@@ -64,18 +109,29 @@ class MoviesAdapter() : RecyclerView.Adapter<MoviesAdapter.MyViewHolder>() {
     }
 
 
+    override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
 
-    override fun onBindViewHolder(holder: MyViewHolder,  position: Int) {
         holder.bindMovie(movies[position])
+        val movie: Result = movies.get(position)
+
+        holder.itemView.setOnClickListener {
+
+            onMovieClickListener_object?.onMovieClick(movie)
+
+
+        }
 
 
     }
 
     override fun getItemCount(): Int {
-        return movies.size-19
+        return movies.size - 19
         // Log.d("getItemCount", (movies.size).toString())
     }
 
 
+    interface onMovieClickListener {
+        fun onMovieClick(movie: Result)
+    }
 
 }
