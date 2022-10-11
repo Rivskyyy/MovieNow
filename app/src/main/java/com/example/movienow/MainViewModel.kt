9eeec4190323ee1277170a4e476615bcc2a3ab21
Taras Bandrivskyi <taras.bandrivskyi@gmail.com>
2022-10-11 04:09:05 +0300

    added MovieDetail Activity,MainViewModel

---
+++ app/src/main/java/com/example/movienow/MainViewModel.kt
@@ -0,0 +1,52 @@
+package com.example.movienow
+
+import android.app.Application
+import android.content.Context
+import android.util.Log
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
+import com.example.movienow.API_SERVICE.Result
+import com.example.movienow.database.MovieDao
+import com.example.movienow.database.MovieDataBase
+import io.reactivex.rxjava3.disposables.CompositeDisposable
+import io.reactivex.rxjava3.disposables.Disposable
+import io.reactivex.rxjava3.schedulers.Schedulers
+import kotlinx.coroutines.flow.callbackFlow
+
+class MainViewModel(application: Application) : AndroidViewModel(application) {
+
+    private val compositeDisposable = CompositeDisposable()
+
+    private val movieDao: MovieDao = MovieDataBase.getInstance(application).movieDao()
+
+    fun getFavouriteMovie(movieId: Int?): LiveData<Result> {
+
+        return movieDao.getFavouriteById(movieId)
+    }
+    fun getFavouriteMovieIdfromDB(){
+
+    }
+
+    fun insertMovie(result: Result) {
+        val disposable: Disposable = movieDao.insertMovie(result).subscribeOn(Schedulers.io()).subscribe()
+        Log.d("insert", disposable.toString())
+        compositeDisposable.add(disposable)
+    }
+
+    fun removeMovie(movieId: Int?) {
+        val disposable: Disposable = movieDao
+            .removeMovie(movieId!!)
+            .subscribeOn(Schedulers.io())
+            .subscribe()
+        compositeDisposable.add(disposable)
+    }
+
+    override fun onCleared() {
+        super.onCleared()
+        compositeDisposable.dispose()
+    }
+}
+
+
+
+
---
+++ app/src/main/java/com/example/movienow/MovieDetailActivity.kt
@@ -0,0 +1,128 @@
+package com.example.movienow
+
+import android.content.Context
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.widget.ImageButton
+import android.widget.ImageView
+import android.widget.TextView
+import android.widget.Toast
+import androidx.lifecycle.Observer
+import com.bumptech.glide.Glide
+import com.example.movienow.API_SERVICE.BASE_IMAGE_POSTER
+import com.example.movienow.API_SERVICE.Result
+import com.example.movienow.adapter.MoviesAdapter
+import com.example.movienow.database.MovieDao
+import com.example.movienow.database.MovieDataBase
+import io.reactivex.rxjava3.schedulers.Schedulers
+
+const val EXTRA_MOVIE: String = "movie"
+
+class MovieDetailActivity : AppCompatActivity() {
+
+    private lateinit var posterImageView: ImageView
+    private lateinit var nameOfMovie: TextView
+    private lateinit var yearOfMovie: TextView
+    private lateinit var descriptionOfMovie: TextView
+    private lateinit var rating: TextView
+    private lateinit var adapter: MoviesAdapter
+
+    // private lateinit var movieDao : MovieDao
+    private lateinit var imageFavorite: ImageButton
+    private lateinit var viewModel: MainViewModel
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_movie_detail)
+        initView()
+
+        var movie: Result? = null
+        var movieId: Int? = intent.getIntExtra("movie.id", 0)
+        var movieTitle = intent.getStringExtra("movie.title")
+        var movieDescription = intent.getStringExtra("movie.overview")
+        var movieRating = intent.getDoubleExtra("movie.rating", 0.0)
+        var moviePoster = intent.getStringExtra("movie.poster")
+        var movieYear = intent.getStringExtra("movie.year")
+
+
+
+        Log.d("MOVIE_ID", movieId.toString())
+        // var movieInfo  = movieId; movieTitle; movieDescription; movieRating; movieRating; moviePoster; movieYear as? Result
+        // Log.d("MOVIE_INFO", movieInfo.toString())
+        //  Log.d("MOVIE_TITLE",movie?.title.toString())
+        //  Log.d("MOVIE_DESCRIPTION", movieDescription?.overview.toString())
+        // Log.d("MOVIE_RATING", movieRating?.vote_average.toString())
+
+        Glide.with(this).load(BASE_IMAGE_POSTER + moviePoster).into(posterImageView)
+        nameOfMovie.text = movieTitle
+        yearOfMovie.text = movieYear.toString()
+        descriptionOfMovie.text = movieDescription
+        rating.text = movieRating.toString()
+
+
+
+        viewModel = MainViewModel(application)
+        viewModel.getFavouriteMovie(movieId)?.observe(this, Observer {
+
+            if (it?.id == null) {
+                imageFavorite.setImageResource(R.drawable.heart_icon_empty_resize)
+
+                imageFavorite.setOnClickListener {
+                    if (movieId != null) {
+                        viewModel.insertMovie(
+                            Result(
+                                movieId,
+                                movieDescription,
+                                moviePoster,
+                                movieYear,
+                                movieTitle,
+                                movieRating
+                            )
+                        )
+                    }
+                    Toast.makeText(this, "Added", Toast.LENGTH_SHORT).show()
+
+                }
+            } else {
+                imageFavorite.setImageResource(R.drawable.heart_button_red)
+                imageFavorite.setOnClickListener {
+                    viewModel.removeMovie(movieId)
+                    Toast.makeText(this, "deleted", Toast.LENGTH_SHORT).show()
+                }
+            }
+        })
+
+
+    }
+
+
+    companion object {
+        fun NewIntent(context: Context, movie: Result): Intent {
+
+            var intent: Intent = Intent(context, MovieDetailActivity::class.java)
+            //   intent.putExtra("movie", movie)
+            intent.putExtra("movie.id", movie.id)
+            intent.putExtra("movie.overview", movie.overview)
+            intent.putExtra("movie.title", movie.title)
+            intent.putExtra("movie.poster", movie.poster_path)
+            intent.putExtra("movie.year", movie.release_date)
+            intent.putExtra("movie.rating", movie.vote_average)
+            return intent
+        }
+    }
+
+
+    private fun initView() {
+        posterImageView = findViewById(R.id.posterImageDetail)
+        nameOfMovie = findViewById(R.id.nameOfMovieDetail)
+        yearOfMovie = findViewById(R.id.yearOfMovieDetail)
+        descriptionOfMovie = findViewById(R.id.descriptionOfMovieDetail)
+        rating = findViewById(R.id.ratingDetail)
+        imageFavorite = findViewById(R.id.imageButtonFavor)
+
+    }
+
+
+}
