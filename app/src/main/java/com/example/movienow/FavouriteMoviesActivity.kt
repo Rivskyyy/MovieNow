6d3f5befa7488c018503be31b44e21b821cdae08
Taras Bandrivskyi <taras.bandrivskyi@gmail.com>
2022-10-19 11:56:55 +0300

    Added new button, textView

--- app/src/main/java/com/example/movienow/FavouriteMoviesActivity.kt
+++ app/src/main/java/com/example/movienow/FavouriteMoviesActivity.kt
@@ -4,7 +4,11 @@ import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import android.util.Log
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.isEmpty
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.GridLayoutManager
@@ -13,17 +17,28 @@ import com.RivskyInc.movienow.API_SERVICE.Result
 import com.RivskyInc.movienow.adapter.FavouriteMoviesAdapter
 
 class FavouriteMoviesActivity : AppCompatActivity() {
-
+    private lateinit var textViewEmpty : TextView
+    private lateinit var backButton : Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_favourite_movies)
 
+        textViewEmpty = findViewById(R.id.textViewEmpty)
+        backButton = findViewById(R.id.buttonBackFromFavourite)
+        backButton.setOnClickListener{  finish()}
+
+
         var recyclerView: RecyclerView = findViewById(R.id.recyclerViewMovies)
         var moviesAdapter: FavouriteMoviesAdapter = FavouriteMoviesAdapter()
 
+
+
         recyclerView.layoutManager = GridLayoutManager(this, 2)
         recyclerView.adapter = moviesAdapter
+//        if (moviesAdapter.setMovies() ){
+//            textViewEmpty.setVisibility(View.VISIBLE)
+//        }
 
         var viewModel: FavouriteViewModel = ViewModelProvider(this)[FavouriteViewModel::class.java]
         moviesAdapter.onMovieClickListener_object = object : FavouriteMoviesAdapter.onMovieClickListener {
@@ -38,6 +53,9 @@ class FavouriteMoviesActivity : AppCompatActivity() {
 
             viewModel.getMovies().observe(this, Observer {
                var showMovies =  moviesAdapter.setMovies(it)
+                if (it.isEmpty()){
+                    textViewEmpty.setVisibility(View.VISIBLE)
+                }
                 Log.d("show_favourite", showMovies.toString())
             })
 
